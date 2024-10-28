333ee3b0795e5b4ad48f0598f6c18e616af16940
sad-sys <khawajasadiq25@gmail.com>
2024-10-28 17:00:14 +0000

    First unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,32 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
 
-print (browser.title)
-assert 'Django' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        'Johnson opens up a website to write a To Do list'
+        'He goes to the homepage'
+        self.browser.get("http://localhost:8000")
+
+        'J notices the page and title holds the phrase "Add To-Do list" '
+        self.assertIn("To-Do", self.browser.title)
+        self.fail('Finish the test now')
+
+        'He adds book to a text box' 
+        'It shows up underneath immediately'
+
+        'Checking if it saved the right item'
+
+        'He revisits that URL to see if the item is still there'
+
+        'He closes the browser'
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
